6d0a536ff0a2539444e99f45319b42bb2737d923
Leif Johnson <lmjohns3@gmail.com>
2022-01-15 21:43:46 -0800

    Replace hard-coded config values with command line arguments.

--- mqtt-to-timescale/index.js
+++ mqtt-to-timescale/index.js
@@ -1,19 +1,18 @@
+import arg from 'arg'
 import { connect } from 'mqtt'
 import pg from 'pg'
 
-const sql = new pg.Client({
-    user: 'bot',
-    password: 'ewxdyoyoLuv4trfRyJMfFyNFVLgduvLm',
-    database: 'bolinas',
-})
+const args = arg({ '--pg': String, '--mqtt': String, '--sub': String })
+
+const sql = new pg.Client(args['--pg'])
 
 sql.connect(err => {
   if (err) throw err;
 
-  const mqtt  = connect('mqtt://ok', { username: 'ok', password: '0c:c4:7a:b0:5d:68' })
+  const mqtt  = connect(args['--mqtt'] || 'mqtt://localhost')
 
   mqtt.on('connect', () => {
-    mqtt.subscribe('bo/mon/+/+', (err, conns) => {
+    mqtt.subscribe(args['--sub'] || '#', (err, conns) => {
       console.log('subscribed to', conns[0]);
     })
   })
